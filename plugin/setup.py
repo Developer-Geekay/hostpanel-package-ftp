5cd2758273c06cb0a361f9f98f3f0bef10b679f3
Developer-Geekay <developergeekay@gmail.com>
2026-06-17 00:21:22 +0300

    chore: bump version to 1.1.4

--- plugin/setup.py
+++ plugin/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="hostpanel-ftp",
-    version="1.1.3",
+    version="1.1.4",
     packages=find_packages(),
     install_requires=[
         "fastapi",
